bbc0cd48b5a8631f94960618b35b44aefede6e33
tom-bandiera <45265799+tom-bandiera@users.noreply.github.com>
2024-03-15 15:26:44 +0100

    Slime Idle & Walk states and animations transitions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D body;
     public BoxCollider2D groundCheck;
+
     [Range(0f, 1f)]
     [SerializeField] private float groundDecay;
     [SerializeField] private float groundSpeed;
@@ -19,6 +20,7 @@ public class Player : MonoBehaviour
 
     private float xInput;
     private float yInput;
+    private bool isWalking;
 
     void Start()
     {
@@ -45,6 +47,8 @@ public class Player : MonoBehaviour
             float increment = xInput * acceleration;
             float newSpeed = Mathf.Clamp(body.velocity.x + increment, -groundSpeed, groundSpeed);
             body.velocity = new Vector2(xInput * groundSpeed, body.velocity.y);
+
+            isWalking = Mathf.Abs(body.velocity.x) > 0.3f;
 
             // Face direction we are moving on X
             float direction = Mathf.Sign(xInput);
@@ -79,5 +83,10 @@ public class Player : MonoBehaviour
         {
             body.velocity = new Vector2(body.velocity.x, jumpSpeed);
         }
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
